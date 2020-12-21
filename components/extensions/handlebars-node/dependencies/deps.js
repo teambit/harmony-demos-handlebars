5a60aa57f6472df431855636ab07a22155fb800c
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-12-21 21:05:46 +0200

    more deps

--- components/extensions/handlebars-node/dependencies/deps.js
+++ components/extensions/handlebars-node/dependencies/deps.js
@@ -1,12 +1,16 @@
 export const deps = {
     dependencies: {
+      react: '-',
     },
     // TODO: add this only if using ts
     devDependencies: {
-
+      '@types/react': '16.9.43',
+      '@types/react-router-dom': '^5.1.5',
     },
     // TODO: take version from config
     peerDependencies: {    
+      react: '^16.13.1',
+      'react-dom': '^16.13.1',
     //   "@testing-library/jest-dom": "^5.11.3",
     //   "enzyme-to-json": {
     //     "version": "3.6.1",
