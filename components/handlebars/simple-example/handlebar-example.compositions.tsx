0d10a4039e2591207cc87d8df73363f9d93f32f8
Benjamin Gilbert <benjamingilbert@Benjamins-MacBook-Pro.local>
2020-12-21 11:40:58 +0200

    add first handlebar component

---
+++ components/handlebars/simple-example/handlebar-example.compositions.tsx
@@ -0,0 +1,9 @@
+import { HbExample } from './index';
+
+export const HandlebarExampleConsumer = () => {
+    return (
+      <div style={{ width: 400 }}>
+        <HbExample message="works with Bit!!!" />
+      </div>
+    )
+  }
